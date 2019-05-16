717f9c0a9a1e71152e00a23d71b11c2e432d6acb
smhrd <smhrd@DESKTOP-86019NH>
2019-05-16 17:05:32 +0900

    ㅁㄴㅇㄻㄴㄹㅇ

--- TimProject/src/P2_View/Btn1_Button2.java
+++ TimProject/src/P2_View/Btn1_Button2.java
@@ -17,13 +17,13 @@ import javax.swing.table.DefaultTableModel;
 import javax.swing.JScrollPane;
 import java.awt.Cursor;
 
-public class Lobby extends JPanel {
+public class Btn1_Button2 extends JPanel {
 	private JTextField textField;
 
 	/**
 	 * Create the panel.
 	 */
-	public Lobby() {
+	public Btn1_Button2() {
 		setLayout(new CardLayout(0, 0));
 
 		JPanel Lobby_Panel = new JPanel();
@@ -48,7 +48,7 @@ public class Lobby extends JPanel {
 		lbl_program_name.setFont(new Font("°í¾çÃ¼", Font.PLAIN, 20));
 		lbl_program_name.setForeground(new Color(255, 255, 255));
 		ProgramNamePanel.add(lbl_program_name, "name_53851796879200");
-		
+
 		JPanel ProfilePanel = new JPanel();
 		ProfilePanel.setBackground(new Color(71, 120, 197));
 		ProfilePanel.setBounds(0, 158, 280, 100);
@@ -76,20 +76,25 @@ public class Lobby extends JPanel {
 		label_1.setFont(new Font("KBIZÇÑ¸¶À½°íµñ B", Font.PLAIN, 16));
 		label_1.setBounds(30, 38, 100, 25);
 		Profile_Nickname.add(label_1);
-		
+
 		JPanel btn_1 = new JPanel();
 		btn_1.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		btn_1.setBackground(new Color(41, 57, 80));
+		btn_1.setBackground(new Color(255, 255, 255,20));
 		btn_1.setBounds(0, 361, 300, 80);
 		LeftLobbyPanel.add(btn_1);
 		btn_1.setLayout(null);
-		
+
 		JLabel lbl_btn1 = new JLabel("\uC120\uC218\uAD00\uB9AC");
 		lbl_btn1.setBounds(87, 30, 87, 24);
 		lbl_btn1.setFont(new Font("KBIZÇÑ¸¶À½°íµñ H", Font.PLAIN, 20));
 		lbl_btn1.setForeground(new Color(255, 255, 255));
 		btn_1.add(lbl_btn1);
-		
+
+		JPanel btn4_deco = new JPanel();
+		btn4_deco.setBounds(0, 0, 5, 80);
+		btn_1.add(btn4_deco);
+		btn4_deco.setBackground(Color.WHITE);
+
 		JPanel ExitPanel = new JPanel();
 		ExitPanel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		ExitPanel.addMouseListener(new MouseAdapter() {
@@ -102,144 +107,200 @@ public class Lobby extends JPanel {
 		ExitPanel.setBounds(0, 820, 300, 80);
 		LeftLobbyPanel.add(ExitPanel);
 		ExitPanel.setLayout(null);
-		
+
 		JPanel ExitIconPanel = new JPanel();
 		ExitIconPanel.setBackground(new Color(41, 57, 80));
 		ExitIconPanel.setBounds(0, 0, 76, 80);
 		ExitPanel.add(ExitIconPanel);
 		ExitIconPanel.setLayout(new CardLayout(0, 0));
-		
+
 		JLabel lblNewLabel_1 = new JLabel("");
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_1.setIcon(new ImageIcon(Lobby.class.getResource("/P5_Img/icons8_exit_32px.png")));
+		lblNewLabel_1.setIcon(new ImageIcon(Btn1_Button2.class.getResource("/P5_Img/icons8_exit_32px.png")));
 		ExitIconPanel.add(lblNewLabel_1, "name_58332631504300");
-		
+
 		JPanel panel_1 = new JPanel();
 		panel_1.setBackground(new Color(41, 57, 80));
 		panel_1.setBounds(74, 0, 226, 80);
 		ExitPanel.add(panel_1);
 		panel_1.setLayout(null);
-		
+
 		JLabel lblNewLabel_2 = new JLabel("\uB098\uAC00\uAE30");
 		lblNewLabel_2.setBounds(0, 10, 111, 60);
-		lblNewLabel_2.setFont(new Font("KBIZÇÑ¸¶À½°íµñ B", Font.BOLD, 20));
+		lblNewLabel_2.setFont(new Font("KBIZÇÑ¸¶À½°íµñ H", Font.BOLD, 20));
 		lblNewLabel_2.setForeground(new Color(255, 255, 255));
 		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
 		panel_1.add(lblNewLabel_2);
-		
+
 		JPanel SearchPanel = new JPanel();
-		SearchPanel.setBackground(new Color(41,57,80));
+		SearchPanel.setBackground(new Color(41, 57, 80));
 		SearchPanel.setBounds(0, 59, 300, 54);
 		LeftLobbyPanel.add(SearchPanel);
 		SearchPanel.setLayout(null);
-		
+
 		JPanel SearchIconPanel = new JPanel();
 		SearchIconPanel.setBounds(12, 10, 32, 32);
-		SearchIconPanel.setBackground(new Color(41,57,80));
+		SearchIconPanel.setBackground(new Color(41, 57, 80));
 		SearchPanel.add(SearchIconPanel);
 		SearchIconPanel.setLayout(null);
-		
+
 		JLabel label = new JLabel("");
 		label.setBounds(0, 0, 32, 32);
 		label.setHorizontalAlignment(SwingConstants.CENTER);
-		label.setIcon(new ImageIcon(Lobby.class.getResource("/P5_Img/icons8_search_32px.png")));
-		
+		label.setIcon(new ImageIcon(Btn1_Button2.class.getResource("/P5_Img/icons8_search_32px.png")));
+
 		SearchIconPanel.add(label);
-		
+
 		JPanel SearchTxtPanel = new JPanel();
 		SearchTxtPanel.setBounds(46, 0, 254, 68);
-		SearchTxtPanel.setBackground(new Color(41,57,80));
+		SearchTxtPanel.setBackground(new Color(41, 57, 80));
 		SearchPanel.add(SearchTxtPanel);
 		SearchTxtPanel.setLayout(null);
-		
+
 		textField = new JTextField();
-		textField.setFont(new Font("KBIZÇÑ¸¶À½°íµñ B", Font.PLAIN, 14));
+		textField.setFont(new Font("KBIZÇÑ¸¶À½°íµñ H", Font.PLAIN, 14));
 		textField.setForeground(new Color(255, 255, 255));
 		textField.setBorder(null);
 		textField.setBackground(new Color(41, 57, 80));
 		textField.setBounds(12, 10, 210, 28);
 		SearchTxtPanel.add(textField);
 		textField.setColumns(10);
-		
+
 		JSeparator separator = new JSeparator();
 		separator.setForeground(new Color(255, 255, 255));
 		separator.setBounds(12, 49, 210, 2);
 		SearchTxtPanel.add(separator);
-		
+
 		JPanel btn_2 = new JPanel();
 		btn_2.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		btn_2.setLayout(null);
 		btn_2.setBackground(new Color(41, 57, 80));
 		btn_2.setBounds(0, 439, 300, 80);
 		LeftLobbyPanel.add(btn_2);
-		
+
 		JLabel lbl_btn2 = new JLabel("\uAD6C\uB2E8\uAD00\uB9AC");
 		lbl_btn2.setForeground(Color.WHITE);
 		lbl_btn2.setFont(new Font("KBIZÇÑ¸¶À½°íµñ H", Font.PLAIN, 20));
 		lbl_btn2.setBounds(87, 30, 87, 24);
 		btn_2.add(lbl_btn2);
-		
+
 		JPanel btn_3 = new JPanel();
 		btn_3.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		btn_3.setLayout(null);
 		btn_3.setBackground(new Color(41, 57, 80));
 		btn_3.setBounds(0, 517, 300, 80);
 		LeftLobbyPanel.add(btn_3);
-		
+
 		JLabel lbl_btn3 = new JLabel("\uC774\uC801\uAD00\uB9AC");
 		lbl_btn3.setForeground(Color.WHITE);
 		lbl_btn3.setFont(new Font("KBIZÇÑ¸¶À½°íµñ H", Font.PLAIN, 20));
 		lbl_btn3.setBounds(87, 30, 87, 24);
 		btn_3.add(lbl_btn3);
-		
+
 		JPanel btn4 = new JPanel();
 		btn4.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		btn4.setLayout(null);
 		btn4.setBackground(new Color(41, 57, 80));
 		btn4.setBounds(0, 595, 300, 80);
 		LeftLobbyPanel.add(btn4);
-		
+
 		JLabel lbl_btn4 = new JLabel("\uAC1C\uC778\uC124\uC815");
 		lbl_btn4.setForeground(Color.WHITE);
 		lbl_btn4.setFont(new Font("KBIZÇÑ¸¶À½°íµñ H", Font.PLAIN, 20));
 		lbl_btn4.setBounds(87, 30, 87, 24);
 		btn4.add(lbl_btn4);
+
+		JPanel PlayerButtonPanel = new JPanel();
+		PlayerButtonPanel.setBackground(new Color(71, 120, 197));
+		PlayerButtonPanel.setBounds(280, 0, 1320, 105);
+		Lobby_Panel.add(PlayerButtonPanel);
+		PlayerButtonPanel.setLayout(null);
+
+		JPanel AllPlayerButton = new JPanel();
+		AllPlayerButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		AllPlayerButton.setBackground(new Color(71, 120, 197));
+		AllPlayerButton.setBounds(130, 0, 220, 105);
+		PlayerButtonPanel.add(AllPlayerButton);
+		AllPlayerButton.setLayout(null);
 		
-		JPanel GudanPanel = new JPanel();
-		GudanPanel.setBackground(new Color(71, 120, 197));
-		GudanPanel.setBounds(280, 0, 1320, 105);
-		Lobby_Panel.add(GudanPanel);
-		GudanPanel.setLayout(null);
+		JLabel lbl_AllPlayerButton = new JLabel("\uC804\uCCB4\uBCF4\uAE30");
+		lbl_AllPlayerButton.setBounds(0, 0, 220, 105);
+		lbl_AllPlayerButton.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl_AllPlayerButton.setForeground(new Color(255, 255, 255));
+		lbl_AllPlayerButton.setFont(new Font("KBIZÇÑ¸¶À½°íµñ H", Font.PLAIN, 20));
+		lbl_AllPlayerButton.setBackground(new Color(255, 255, 255));
+		AllPlayerButton.add(lbl_AllPlayerButton);
 		
-		JPanel LogoPanel = new JPanel();
-		LogoPanel.setBackground(new Color(71, 120, 197));
-		LogoPanel.setBounds(42, 0, 111, 105);
-		GudanPanel.add(LogoPanel);
-		LogoPanel.setLayout(new CardLayout(0, 0));
+		JPanel PlayerStatButton = new JPanel();
+		PlayerStatButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		PlayerStatButton.setBackground(new Color(255, 255, 255,60));
+		PlayerStatButton.setBounds(350, 0, 220, 105);
+		PlayerButtonPanel.add(PlayerStatButton);
+		PlayerStatButton.setLayout(null);
 		
-		JLabel lbl_logo = new JLabel("");
-		lbl_logo.setIcon(new ImageIcon(Lobby.class.getResource("/P5_Img/icons8_uefa_euro_trophy_64px.png")));
-		lbl_logo.setHorizontalAlignment(SwingConstants.CENTER);
-		LogoPanel.add(lbl_logo, "name_60328911748400");
-		
-		JPanel GudanName = new JPanel();
-		GudanName.setBackground(new Color(71, 120, 197));
-		GudanName.setBounds(153, 0, 118, 105);
-		GudanPanel.add(GudanName);
-		GudanName.setLayout(null);
-		
-		JLabel lblNewLabel = new JLabel("\uC6CC\uB9AC\uC5B4\uC988");
-		lblNewLabel.setBounds(0, 31, 146, 34);
-		lblNewLabel.setForeground(new Color(255, 255, 255));
-		lblNewLabel.setFont(new Font("KBIZÇÑ¸¶À½°íµñ H", Font.PLAIN, 25));
-		GudanName.add(lblNewLabel);
+		JLabel lbl_PlayerStatButton = new JLabel("\uC120\uC218\uC2A4\uD0EF");
+		lbl_PlayerStatButton.setBounds(0, 0, 220, 105);
+		lbl_PlayerStatButton.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl_PlayerStatButton.setForeground(Color.WHITE);
+		lbl_PlayerStatButton.setFont(new Font("KBIZÇÑ¸¶À½°íµñ H", Font.PLAIN, 20));
+		lbl_PlayerStatButton.setBackground(Color.WHITE);
+		PlayerStatButton.add(lbl_PlayerStatButton);
 		
 		JPanel panel = new JPanel();
-		panel.setBackground(new Color(255, 255, 255));
-		panel.setBounds(280, 104, 1320, 796);
-		Lobby_Panel.add(panel);
-		panel.setLayout(null);
+		panel.setBounds(0, 98, 220, 7);
+		PlayerStatButton.add(panel);
 		
+		JPanel PlayerChart = new JPanel();
+		PlayerChart.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		PlayerChart.setBackground(new Color(71, 120, 197));
+		PlayerChart.setBounds(570, 0, 220, 105);
+		PlayerButtonPanel.add(PlayerChart);
+		PlayerChart.setLayout(new CardLayout(0, 0));
+		
+		JLabel lbl_PlayerChart = new JLabel("\uCC28\uD2B8\uBCF4\uAE30");
+		lbl_PlayerChart.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl_PlayerChart.setForeground(Color.WHITE);
+		lbl_PlayerChart.setFont(new Font("KBIZÇÑ¸¶À½°íµñ H", Font.PLAIN, 20));
+		lbl_PlayerChart.setBackground(Color.WHITE);
+		PlayerChart.add(lbl_PlayerChart, "name_3777310714400");
+		
+		JPanel PlayerMemoPanel = new JPanel();
+		PlayerMemoPanel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		PlayerMemoPanel.setBackground(new Color(71, 120, 197));
+		PlayerMemoPanel.setBounds(790, 0, 220, 105);
+		PlayerButtonPanel.add(PlayerMemoPanel);
+		PlayerMemoPanel.setLayout(new CardLayout(0, 0));
+		
+		JLabel lbl_PlayerMemo = new JLabel("\uD3C9\uAC00\uBA54\uBAA8");
+		PlayerMemoPanel.add(lbl_PlayerMemo, "name_3775184187700");
+		lbl_PlayerMemo.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl_PlayerMemo.setForeground(Color.WHITE);
+		lbl_PlayerMemo.setFont(new Font("KBIZÇÑ¸¶À½°íµñ H", Font.PLAIN, 20));
+		lbl_PlayerMemo.setBackground(Color.WHITE);
+
+		JPanel StatViewPanel = new JPanel();
+		StatViewPanel.setBackground(new Color(255, 255, 255));
+		StatViewPanel.setBounds(280, 104, 1320, 796);
+		Lobby_Panel.add(StatViewPanel);
+		StatViewPanel.setLayout(null);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBounds(52, 47, 100, 100);
+		StatViewPanel.add(panel_2);
+		panel_2.setLayout(new CardLayout(0, 0));
+		
+		JLabel lblNewLabel = new JLabel("\uC0AC\uC9C4");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_2.add(lblNewLabel, "name_5083844218100");
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBounds(164, 47, 302, 100);
+		StatViewPanel.add(panel_3);
+		panel_3.setLayout(null);
+		
+		JLabel lblNewLabel_3 = new JLabel("New label");
+		lblNewLabel_3.setBounds(12, 10, 110, 33);
+		panel_3.add(lblNewLabel_3);
 
 	}
 }
